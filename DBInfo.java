d111feca6e01efa6f0de373b7cc660729fab3db6
Santosh Kumar Sethy <154328801+santoshksethy@users.noreply.github.com>
2025-04-06 21:48:04 +0530

    Delete DBInfo.java

--- DBInfo.java
+++
@@ -1,8 +0,0 @@
-package com;
-public interface DBInfo 
-{
-	String DRIVER_NAME="com.mysql.jdbc.Driver";
-	String URL="jdbc:mysql://localhost:3306/customer";
-	String USER_NAME="root";
-	String PASSWORD="Santosh@90"; 
-}
